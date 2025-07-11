70689bcf5fc4145dd7a60bf43ca1ef9b9c5993d1
Moerfelden <96487145+Moerfelden@users.noreply.github.com>
2025-07-11 13:49:20 +0200

    Update models.py

--- modules/location-grpc/app/models.py
+++ modules/location-grpc/app/models.py
@@ -12,6 +12,15 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 base = declarative_base()
 
+class Person(Base):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
+    
+
 class Location(base):
     __tablename__ = "location"
 
