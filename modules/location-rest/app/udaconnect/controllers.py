b01146760c3b00f2defb587a00a1f1b22c1c7413
Moerfelden <96487145+Moerfelden@users.noreply.github.com>
2025-07-17 13:36:46 +0200

    Create controllers.py

---
+++ modules/location-rest/app/udaconnect/controllers.py
@@ -0,0 +1,29 @@
+import logging
+from app.udaconnect.schemas import LocationSchema
+from app.udaconnect.services import LocationService
+from datetime import datetime
+from flask_accepts import accepts
+from flask_restx import Namespace, Resource
+from flask import request, Response
+from typing import Optional, List
+
+api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
+
+
+# TODO: This needs better exception handling
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+@api.route("/locations")
+class LocationResource(Resource):
+    @accepts(schema=LocationSchema)
+    def post(self):
+        try:
+            payload = request.get_json()
+            logger.info(f"{datetime.now()} A new location is sent to the Kafka broker: {payload}")     
+            LocationService.create(payload)
+        except:
+            logger.error(f"{datetime.now()} An issue occurred sending this new location to the Kafka broker: {payload}")     
+            Response(status=400)
+        return Response(status=202)
